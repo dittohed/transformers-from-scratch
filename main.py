e5dcd9ad7477dff80200e22d2d96c4fb14af7cd8
dittohed <wojtek.konieczkowicz@gmail.com>
2022-08-25 21:00:22 +0200

    Finish up implementing de_to_en training code

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import copy
 import math
+import os
+
+import spacy
 
 import torch
 import torch.nn as nn
-from torch.nn.functional import log_softmax
+import torchtext.datasets as datasets
+import torch.distributed as dist
+
+from torch.nn.functional import log_softmax, pad
+from torchtext.vocab import build_vocab_from_iterator
+from torchtext.data.functional import to_map_style_dataset
 from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data import DataLoader
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+from os.path import exists
 
 from tqdm import tqdm
 
@@ -548,7 +561,7 @@ def load_tokenizers():
         spacy_en = spacy.load('en_core_web_sm')
     except IOError:
         os.system('python -m spacy download en_core_web_sm')
-        spacy_en = spacy.load('de_core_web_sm')
+        spacy_en = spacy.load('en_core_web_sm')
 
     return spacy_de, spacy_en
 
@@ -568,13 +581,226 @@ def build_vocabulary(spacy_de, spacy_en):
     def tokenize_en(text):
         return tokenize(text, spacy_en)
 
+    # TODO: rzeczywiście potrzeba dwa razy brać datasets?
+    print('Building german vocabulary...')
+    train, val, test = datasets.Multi30k(language_pair=('de', 'en'))
+    vocab_src = build_vocab_from_iterator(
+        yield_tokens(train+val+test, tokenize_de, index=1),
+        min_freq=2,
+        specials=['<s>', '</s>', '<blank>', '<unk>']
+    )
 
+    print('Building english vocabulary...')
+    train, val, test = datasets.Multi30k(language_pair=('de', 'en'))
+    vocab_target = build_vocab_from_iterator(
+        yield_tokens(train+val+test, tokenize_en, index=1),
+        min_freq=2,
+        specials=['<s>', '</s>', '<blank>', '<unk>']
+    )
+
+    vocab_src.set_default_index(vocab_src["<unk>"])
+    vocab_target.set_default_index(vocab_target["<unk>"])
+
+    return vocab_src, vocab_target
+
+
+def load_vocab(spacy_de, spacy_en):
+    if not exists('vocab.pt'):
+        vocab_src, vocab_target = build_vocabulary(spacy_de, spacy_en)
+        torch.save((vocab_src, vocab_target), 'vocab.pt')
+    else:
+        vocab_src, vocab_target = torch.load('vocab.pt')
+
+    return vocab_src, vocab_target
+
+
+def collate_batch(batch, src_pipeline, target_pipeline, 
+        src_vocab, target_vocab, device=torch.device('cpu'), 
+        max_padding=128, pad_id=2):
+
+    start_id = torch.tensor([0], device=device)
+    end_id = torch.tensor([1], device=device)
+
+    src_list = []
+    target_list = []
+
+    for (src, target) in batch:
+        src_processed = torch.cat(
+                            [start_id,
+                             torch.tensor(src_vocab(src_pipeline(src)),
+                                          dtype=torch.int64,
+                                          device=device),
+                             end_id], 
+                             0)
+
+        target_processed = torch.cat(
+                            [start_id,
+                             torch.tensor(target_vocab(target_pipeline(target)),
+                                          dtype=torch.int64,
+                                          device=device),
+                             end_id], 
+                             0)
+
+        src_list.append(
+            pad(src_processed, (0, max_padding - len(src_processed)), 
+                value=pad_id)
+        )
+
+        target_list.append(
+            pad(target_processed, (0, max_padding - len(target_processed)), 
+                value=pad_id)
+        )
+
+    src = torch.stack(src_list)
+    target = torch.stack(target_list)
+
+    return (src, target)
+
+
+def create_dataloaders(
+        device, src_vocab, target_vocab, spacy_de, spacy_en, 
+        batch_size=12000, max_padding=128, distributed=True):
+
+    def tokenize_de(text):
+        return tokenize(text, spacy_de)
+
+    def tokenize_en(text):
+        return tokenize(text, spacy_en)
+
+    def collate_fn(batch):
+        return collate_batch(
+            batch,
+            tokenize_de,
+            tokenize_en,
+            src_vocab,
+            device,
+            max_padding=max_padding,
+            pad_id=src_vocab.get_stoi()['<blank>']
+        )
+
+    train_iter, val_iter, test_iter = datasets.Multi30k(
+                                            language_pair=('de', 'en'))
+    train_iter_map = to_map_style_dataset(train_iter)
+    val_iter_map = to_map_style_dataset(val_iter)
+
+    train_sampler = DistributedSampler(train_iter_map) if distributed else None
+    val_sampler = DistributedSampler(val_iter_map) if distributed else None
+
+    train_dataloader = DataLoader(
+        train_iter_map,
+        batch_size=batch_size,
+        shuffle=(train_sampler is None),
+        sampler=train_sampler,
+        collate_fn=collate_fn)
+    val_dataloader = DataLoader(
+        val_iter_map,
+        batch_size=batch_size,
+        shuffle=(val_sampler is None),
+        sampler=val_sampler,
+        collate_fn=collate_fn)
+
+    return (train_dataloader, len(train_iter_map), 
+           val_dataloader, len(val_iter_map))
+
+
+def train_worker(gpu, n_gpus_per_node, src_vocab, target_vocab, 
+        spacy_de, spacy_en, config, distributed=False):
+
+    # print(f'Train worker process using GPU: {gpu} for training', flush=True)
+    # torch.cuda.set_device(gpu)
+
+    pad_idx = target_vocab['<blank>']
+    d_model = 512
+    model = build_model(len(src_vocab), len(target_vocab), n=2)  # TODO: to 6
+    # model.cuda(gpu)
+    module = model 
+    is_main_process = True
+    batch_size = config['batch_size'] // n_gpus_per_node
+
+    if distributed:
+        dist.init_process_group(
+            'nccl', init_method='env://', rank=gpu, world_size=n_gpus_per_node)
+
+        model = DDP(model, device_ids=[gpu])
+        module = model.module 
+        is_main_process = (gpu == 0)
+
+    criterion = LabelSmoothing(n_classes=len(target_vocab), 
+                               padding_idx=pad_idx,
+                               smoothing=0.1)
+    # criterion.cuda(gpu)
+
+    (train_dataloader, n_batches_train,
+    val_dataloader, n_batches_val) = create_dataloaders(
+                                        gpu,
+                                        src_vocab,
+                                        target_vocab,
+                                        spacy_de,
+                                        spacy_en,
+                                        batch_size,
+                                        config['max_padding'],
+                                        distributed)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['base_lr'],
+                    betas=(0.9, 0.98), eps=1e-9)
+    lr_scheduler = LambdaLR(optimizer=optimizer, 
+                            lr_lambda=lambda step: rate(
+                                step, d_model, factor=1, 
+                                warmup_iters=config['warmup']))
+    
+    for epoch in range(config['n_epochs']):
+        if distributed:
+            train_dataloader.sampler.set_epoch(epoch)
+            val_dataloader.sampler.set_epoch(epoch)
+
+        model.train()  # Switch to training mode
+        run_epoch(
+            (Batch(b[0], b[1], pad_idx) for b in train_dataloader),
+            model,
+            SourceTargetCopyLoss(module.head, criterion),
+            optimizer=optimizer,
+            lr_scheduler=lr_scheduler,
+            n_batches=n_batches_train,
+            curr_epoch=epoch,
+            n_epochs=config['n_epochs'],
+            mode='train')
+
+        if is_main_process:
+            pass  # TODO: checkpoint
+
+        model.eval()
+        run_epoch(
+            (Batch(b[0], b[1], pad_idx) for b in val_dataloader),
+            model,
+            SourceTargetCopyLoss(module.head, criterion),
+            n_batches=n_batches_val,
+            mode='eval')
+
+
+def train_de_to_en_model():
+    config = {
+        'batch_size': 8,
+        'distributed': False,
+        'n_epochs': 8,
+        'base_lr': 1.0,
+        'max_padding': 72,
+        'warmup': 3000}
+
+    spacy_de, spacy_en = load_tokenizers()
+    src_vocab, target_vocab = load_vocab(spacy_de, spacy_en)
+
+    if config['distributed']:
+        pass 
+    else:
+        train_worker(
+            0, 1, src_vocab, target_vocab, spacy_de, spacy_en, config, False)
 
 
 if __name__ == '__main__':
     # inference_test()
     # label_smoothing_test()
-    train_copy_task_test()
+    # train_copy_task_test()
+    train_de_to_en_model()
 
 
     
