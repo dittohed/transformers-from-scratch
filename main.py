f0ec4081dd0f7d7538ddae483e30594fb26e50a6
dittohed <wojtek.konieczkowicz@gmail.com>
2022-09-05 16:57:08 +0200

    Move examples to separate file part 2, use device

--- main.py
+++ main.py
@@ -3,55 +3,46 @@ import torch
 from torch.optim.lr_scheduler import LambdaLR
 
 from training import LabelSmoothing, Seq2SeqLoss, rate, run_epoch
-from transformer import build_model
-from utils import Batch, subsequent_mask, decode_greedy
-from lang import load_tokenizers, load_vocab, train_worker
+from transformer import build_encoder_decoder_model
+from utils import Batch, decode_greedy
+from lang import load_tokenizers, load_vocab, train_worker, create_dataloaders
 
 
-def label_smoothing_test():
-    criterion = LabelSmoothing(5, 0, 0.1)
-    preds = torch.Tensor([
-        [0, 0.2, 0.7, 0.1, 0],
-        [0, 0.2, 0.7, 0.1, 0]])
-    
-    criterion(x=preds, target=torch.LongTensor([1, 2]))
-    print(criterion.true_dist)
-
-
-def inference_test():
-    test_model = build_model(11, 11, n_layers=2, n_heads=4)
-    test_model.eval()
-    src = torch.LongTensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
-    src_mask = torch.ones(1, 1, 10)
-
-    memory = test_model.encode(src, src_mask)
-    ys = torch.zeros(1, 1).type_as(src)
-
-    for i in range(9):
-        out = test_model.decode(
-            memory, src_mask, ys, subsequent_mask(ys.size(1)).type_as(src.data)
-        )
-        prob = test_model.head(out[:, -1])
-        _, next_word = torch.max(prob, dim=1)
-        next_word = next_word.data[0]
-        ys = torch.cat(
-            [ys, torch.empty(1, 1).type_as(src.data).fill_(next_word)], dim=1
-        )
-
-    print("Example Untrained Model Prediction:", ys)
-
-
-def train_copy_task():
+def train_copy_task(device):
+    # Hyperparameters
     vocab_size = 11
     batch_size = 64
     n_batches = 32
     n_epochs = 1
 
+    # Data
+    def data_gen(vocab_size, batch_size, n_batches):
+        """
+        Generates random data for a source-to-target copy task.
+        """
+        for i in range(n_batches):
+            # Each rows contains an example of length 10
+            data = torch.randint(1, vocab_size, size=(batch_size, 10))
+            data[:, 0] = 1
+
+            # TODO: try without requires_grad_(False) since detach is used
+            src = data.requires_grad_(False).clone().detach()
+            target = data.requires_grad_(False).clone().detach()
+
+            yield Batch(src, target, 0)
+
+    train_dataloader = data_gen(vocab_size, batch_size, n_batches)
+    n_batches_train = n_batches
+    val_dataloader = data_gen(vocab_size, batch_size, int(n_batches/4))
+    n_batches_val = int(n_batches/4)
+
+    # Model
+    model = build_encoder_decoder_model(
+                vocab_size, vocab_size, n_layers=2).to(device)
+
+    # Training
     criterion = LabelSmoothing(n_classes=vocab_size, padding_idx=0, 
                                smoothing=0)
-
-    model = build_model(vocab_size, vocab_size, n_layers=2)
-
     optimizer = torch.optim.Adam(
                     model.parameters(), lr=0.5, 
                     betas=(0.9, 0.98), eps=1e-9)
@@ -61,27 +52,11 @@ def train_copy_task():
                         step, model.src_preproc[0].d_model, factor=1.0, 
                         warmup_iters=400))
 
-    for epoch in range(n_epochs):
-        model.train()  # Switch to training mode
-        run_epoch(
-            data_gen(vocab_size, batch_size, n_batches),
-            model,
-            Seq2SeqLoss(model.head, criterion),
-            optimizer=optimizer,
-            lr_scheduler=lr_scheduler,
-            n_batches=n_batches,
-            curr_epoch=epoch,
-            n_epochs=n_epochs,
-            mode='train')
+    train_worker(device, model, train_dataloader, val_dataloader,
+        criterion, optimizer, lr_scheduler, 
+        n_batches_train, n_batches_val, n_epochs)
 
-        model.eval()
-        run_epoch(
-            data_gen(vocab_size, batch_size, int(n_batches/4)),
-            model,
-            Seq2SeqLoss(model.head, criterion),
-            n_batches=int(n_batches/4),
-            mode='eval')
-
+    # Eval
     print('=== Post-training test ===')
     model.eval()
     src = torch.LongTensor([list(range(10))])
@@ -92,44 +67,57 @@ def train_copy_task():
     print(f'Prediction: {pred}')
 
 
-def data_gen(vocab_size, batch_size, n_batches):
-    """
-    Generates random data for a source-to-target copy task.
-    """
+def train_de_to_en(device):
+    # Hyperparameters
+    batch_size = 8
+    n_epochs = 8
+    base_lr = 1.0 
+    max_padding = 72
+    warmup = 3000
+    d_model = 512
+    n_layers = 6
 
-    for i in range(n_batches):
-        # Each rows contains an example of length 10
-        data = torch.randint(1, vocab_size, size=(batch_size, 10))
-        data[:, 0] = 1
-
-        # TODO: try without requires_grad_(False) since detach is used
-        src = data.requires_grad_(False).clone().detach()
-        target = data.requires_grad_(False).clone().detach()
-
-        yield Batch(src, target, 0)
-
-
-def train_de_to_en():
-    config = {
-        'batch_size': 8,
-        'distributed': False,
-        'n_epochs': 8,
-        'base_lr': 1.0,
-        'max_padding': 72,
-        'warmup': 3000}
-
+    # Data
     spacy_de, spacy_en = load_tokenizers()
     src_vocab, target_vocab = load_vocab(spacy_de, spacy_en)
+    pad_idx = target_vocab['<blank>']
 
-    if config['distributed']:
-        pass 
-    else:
-        train_worker(
-            None, 1, src_vocab, target_vocab, spacy_de, spacy_en, config, False)
+    (train_dataloader, n_batches_train,
+    val_dataloader, n_batches_val) = create_dataloaders(
+                                        device,
+                                        src_vocab,
+                                        target_vocab,
+                                        spacy_de,
+                                        spacy_en,
+                                        batch_size,
+                                        max_padding
+                                        )
+
+    train_dataloader = (Batch(b[0], b[1], pad_idx) for b in train_dataloader)
+    val_dataloader = (Batch(b[0], b[1], pad_idx) for b in val_dataloader)
+    
+    # Model
+    model = build_encoder_decoder_model(len(src_vocab), len(target_vocab), 
+                                        n_layers).to(device)
+
+    # Training
+    criterion = LabelSmoothing(n_classes=len(target_vocab), 
+                               padding_idx=pad_idx,
+                               smoothing=0.1)
+    optimizer = torch.optim.Adam(model.parameters(), lr=base_lr,
+                    betas=(0.9, 0.98), eps=1e-9)
+    lr_scheduler = LambdaLR(optimizer=optimizer, 
+                            lr_lambda=lambda step: rate(
+                                step, d_model, factor=1, 
+                                warmup_iters=warmup))
+
+    train_worker(device, model, train_dataloader, val_dataloader, 
+        criterion, optimizer, lr_scheduler, 
+        n_batches_train, n_batches_val, n_epochs)
 
 
 if __name__ == '__main__':
-    # inference_test()
-    # label_smoothing_test()
-    train_copy_task()
-    # train_de_to_en()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # train_copy_task(device)
+    train_de_to_en(device)
